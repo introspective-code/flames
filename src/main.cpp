87761d3f1529d7a56b7132db569c3649aa0377a5
Shahriyar Nasir <contact@snasir.ca>
2018-12-26 14:50:24 -0500

    Improve extensibility

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,22 @@ CRGB leds[NUM_LEDS];
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-bool flamesAreOn = false;
+enum Effects { Flames };
+enum SwitchState { On, Off };
+
+Effects effect = Flames;
+SwitchState switchState = Off;
 
 void callback(char *topic, byte *payloadRaw, unsigned int length) {
   payloadRaw[length] = '\0';
   String payload = String((char *)payloadRaw);
-  if (payload == "TURN FLAMES ON") {
-    flamesAreOn = true;
-  } else if (payload == "TURN FLAMES OFF") {
-    flamesAreOn = false;
+
+  if (payload == "Effect: Flames") {
+    effect = Flames;
+  } else if (payload == "Switch: On") {
+    switchState = On;
+  } else if (payload == "Switch: Off") {
+    switchState = Off;
   }
 }
 
@@ -41,12 +48,9 @@ void setup() {
   while (!client.connected()) {
     Serial.println("Connecting to MQTT...");
 
-    if (client.connect("ESP8266Client")) {
-
+    if (client.connect("Headboard Flames")) {
       Serial.println("connected");
-
     } else {
-
       Serial.print("failed with state ");
       Serial.print(client.state());
       delay(2000);
@@ -56,39 +60,52 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   FastLED.setBrightness(50);
 
-  client.subscribe("esp/test");
+  client.subscribe("homie/headboard_flames/strip/effect");
+  client.subscribe("homie/headboard_flames/strip/switch");
+}
+
+void stripSwitchOff() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Black;
+  }
+  FastLED.show();
+}
+
+void flameEffect() {
+  int r = 255;
+  int g = r - 80;
+  int b = 40;
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    int flicker = random(0, 150);
+    int r1 = r - flicker;
+    int g1 = g - flicker;
+    int b1 = b - flicker;
+
+    if (g1 < 0)
+      g1 = 0;
+    if (r1 < 0)
+      r1 = 0;
+    if (b1 < 0)
+      b1 = 0;
+
+    leds[i].red = r1;
+    leds[i].green = g1;
+    leds[i].blue = b1;
+  }
+  delay(random(25, 125));
 }
 
 void loop() {
   client.loop();
 
-  if (flamesAreOn) {
-    int r = 255;
-    int g = r - 80;
-    int b = 40;
+  if (switchState == Off)
+    return stripSwitchOff();
 
-    for (int i = 0; i < NUM_LEDS; i++) {
-      int flicker = random(0, 150);
-      int r1 = r - flicker;
-      int g1 = g - flicker;
-      int b1 = b - flicker;
-
-      if (g1 < 0)
-        g1 = 0;
-      if (r1 < 0)
-        r1 = 0;
-      if (b1 < 0)
-        b1 = 0;
-
-      leds[i].red = r1;
-      leds[i].green = g1;
-      leds[i].blue = b1;
-    }
-    delay(random(25, 125));
-  } else {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB::Black;
-    }
+  switch (effect) {
+  case Flames:
+    flameEffect();
+    break;
   }
 
   FastLED.show();
